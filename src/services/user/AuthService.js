ba64e28b0188d4a84f4783b74d1b7652511f37c0
saimain-primary <saimain.primary@gmail.com>
2023-02-20 21:04:51 +0700

    forget

--- src/services/user/AuthService.js
+++ src/services/user/AuthService.js
@@ -252,17 +252,16 @@ module.exports.forgetPassword = async (data) => {
                     ),
                 });
 
+                // send otp sms here
+
+                let smsResponse = await sendSMS({
+                    phone: phone,
+                    message: `${code} is your BFF reset code. Your code is valid for 3 minute.`,
+                });
+
+                console.log("sms response", smsResponse);
                 console.log("otp saved", otpSaved);
             }
-
-            // send otp sms here
-
-            let smsResponse = await sendSMS({
-                phone: phone,
-                message: `${code} is your BFF reset code. Your code is valid for 3 minute.`,
-            });
-
-            console.log('sms response', smsResponse);
 
             resolve("We have sent you the reset code");
         }
